1dd5016def70d401c135b21158c50a1f179286fd
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-30 21:14:31 +0530

    completed the Job card section

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_app_ui/job_card.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({Key? key}) : super(key: key);
@@ -12,88 +13,155 @@ class MyHomeState extends State<MyHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade400,
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        const SizedBox(
-          height: 50,
-        ),
-        //app bar
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade300,
-                  borderRadius: BorderRadius.circular(4),
-                  border: Border.all(color: Colors.white)),
-              child: Image.asset(
-                'assets/align-left.png',
-                height: 40,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          //app bar
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(4),
+                    border: Border.all(color: Colors.white)),
+                child: Image.asset(
+                  'assets/align-left.png',
+                  height: 40,
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 40,
-        ),
+          const SizedBox(
+            height: 40,
+          ),
 
-        // discover a new path
-        Padding(
-          padding: EdgeInsets.only(left: 25.0),
-          child: Text(
-            'Discover a New Path',
-            style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
+          // discover a new path
+          Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: Text(
+              'Discover a New Path',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-        ),
-        // search bar
-        SizedBox(
-          height: 25,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(5)),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.search,
+          // search bar
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'Search for a job..',
                       ),
-                      border: InputBorder.none,
-                      hintText: 'Search for a job..',
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Container(
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade800,
-                    borderRadius: BorderRadius.circular(5)),
-                child: Image.asset(
-                  'assets/options.png',
-                  color: Colors.white,
-                  width: 36,
+                SizedBox(
+                  width: 10,
                 ),
-              )
-            ],
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade800,
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Image.asset(
+                    'assets/options.png',
+                    color: Colors.white,
+                    width: 36,
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+          SizedBox(
+            height: 25,
+          ),
 
-        // for you section
+          // for you section
 
-        // recently added
-      ]),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              'For You',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+
+          Expanded(
+            // height: 200,
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  JobCard(
+                    imagePath: 'assets/uber.png',
+                    time: 'Full Time',
+                    role: 'UI/UX Designer',
+                    salary: '\$45/h',
+                  ),
+                  JobCard(
+                    imagePath: 'assets/google.png',
+                    time: 'Part Time',
+                    role: 'Product Developer',
+                    salary: '\$80/h',
+                  ),
+                  JobCard(
+                    imagePath: 'assets/apple.png',
+                    time: 'Full Time',
+                    role: 'Software Eng.',
+                    salary: '\$95/h',
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // recently added
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              'Recently Added',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Expanded(
+            // height: 300,
+            flex: 2,
+            child: ListView(
+              children: [Container()],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/job_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class JobCard extends StatelessWidget {
+  const JobCard({
+    Key? key,
+    required this.imagePath,
+    required this.time,
+    required this.role,
+    required this.salary,
+  }) : super(key: key);
+  final String imagePath;
+  final String time;
+  final String role;
+  final String salary;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0),
+      child: Container(
+        // color: Colors.grey.shade800,
+        decoration: BoxDecoration(
+            color: imagePath == 'assets/uber.png'
+                ? Colors.grey.shade800
+                : Colors.white,
+            borderRadius: BorderRadius.circular(10)),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    imagePath,
+                    height: 40,
+                  ),
+                  const SizedBox(
+                    width: 60,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade600,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Text(
+                      time,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    role,
+                    style: TextStyle(
+                      color: imagePath == 'assets/uber.png'
+                          ? Colors.white
+                          : Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(salary, style: TextStyle(color: Colors.grey))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
